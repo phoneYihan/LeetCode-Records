a05ecbd225dd50dd40ae0a07729a1c067ddefdc2
FengYihan <1917460028@qq.com>
2026-02-14 17:00:36 +0800

    接雨水补充_2

--- Hot100/Hot100-7-双指针-接雨水.java
+++ Hot100/Hot100-7-双指针-接雨水.java
@@ -1,6 +1,19 @@
-//双指针解法：（是基于动态规划的改进）
+//双指针解法：（是基于动态规划的改进，优化了空间复杂度）
 /*
+使用双指针和两个变量代替两个数组。
 
+维护两个指针 left 和 right，以及两个变量 leftMax 和 rightMax，初始时 left=0,right=n−1,leftMax=0,rightMax=0；
+指针 left 只会向右移动，指针 right 只会向左移动，在移动指针的过程中维护两个变量 leftMax 和 rightMax 的值。
+
+使用 height[left] 和 height[right] 的值更新 leftMax 和 rightMax 的值；
+
+如果 height[left]<height[right]，则必有 leftMax<rightMax，下标 left 处能接的雨水量等于 leftMax−height[left]，左指针右移；
+
+如果 height[left]≥height[right]，则必有 leftMax≥rightMax，下标 right 处能接的雨水量等于 rightMax−height[right]，右指针左移；
+
+当两个指针相遇时，即可得到能接的雨水总量。
+
+//每次只计算当前一列接到的雨水有多少。
 */
 class Solution {
     public int trap(int[] height) {
@@ -61,5 +74,11 @@ class Solution {
         return ans;
     }
 }
+
+
+
+
+//单调栈解法
+
 
 
