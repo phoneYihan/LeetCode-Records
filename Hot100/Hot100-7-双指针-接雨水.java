99fde94e804a471eeacae0da10a5f7c3bdd9cad8
FengYihan <1917460028@qq.com>
2026-02-15 14:01:20 +0800

    接雨水补充提交_单调栈。

--- Hot100/Hot100-7-双指针-接雨水.java
+++ Hot100/Hot100-7-双指针-接雨水.java
@@ -79,6 +79,29 @@ class Solution {
 
 
 //单调栈解法
-
+/*
+学到栈的时候再来看一下。
+*/
+class Solution {
+    public int trap(int[] height) {
+        int ans = 0;
+        Deque<Integer> stack = new LinkedList<Integer>();
+        int n = height.length;
+        for (int i = 0; i < n; ++i) {
+            while (!stack.isEmpty() && height[i] > height[stack.peek()]) {
+                int top = stack.pop();
+                if (stack.isEmpty()) {
+                    break;
+                }
+                int left = stack.peek();
+                int currWidth = i - left - 1;
+                int currHeight = Math.min(height[left], height[i]) - height[top];
+                ans += currWidth * currHeight;
+            }
+            stack.push(i);
+        }
+        return ans;
+    }
+}
 
 
