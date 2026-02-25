ae2dc12344d31294b96f735e38c79ee656294562
FengYihan <1917460028@qq.com>
2026-02-25 16:33:19 +0800

    第八题-官方题解代码

--- Hot100/Hot100-8-滑动窗口-无重复最长子串.java
+++ Hot100/Hot100-8-滑动窗口-无重复最长子串.java
@@ -16,6 +16,30 @@
 --还需要使用哈希集合来判断 是否有重复的字符。
 **在左指针向右移动的时候，我们从哈希集合中移除一个字符，在右指针向右移动的时候，我们往哈希集合中添加一个字符。
 */
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        // 哈希集合，记录每个字符是否出现过
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        // rk是右指针，初始值为 -1，相当于我们在字符串的左边界的左侧，还没有开始移动
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (i != 0) {
+                // i是左指针。左指针每向右移动一格，就移除一个字符。
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                // 不断地移动右指针
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            // 第 i 到 rk 个字符是一个极长的无重复字符子串
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+    }
+}
+
 
 
 
