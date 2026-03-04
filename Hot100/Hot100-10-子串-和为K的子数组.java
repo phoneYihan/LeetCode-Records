fcde7366b4483e6fff88ecfee81b81163e11fb1b
FengYihan <1917460028@qq.com>
2026-03-04 16:51:46 +0800

    还是灵神的答案更直观。

--- Hot100/Hot100-10-子串-和为K的子数组.java
+++ Hot100/Hot100-10-子串-和为K的子数组.java
@@ -20,6 +20,33 @@ ai + ai-1 + ai-2 = Si+1 - Si-2
 
 */
 
+//灵茶题解
+/*
+
+*/
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        int[] s = new int[n + 1];
+        for (int i = 0; i < n; i++) {
+            s[i + 1] = s[i] + nums[i];
+        }
+
+        Map<Integer, Integer> cnt = new HashMap<>(n + 1, 1); // 预分配空间
+        int ans = 0;
+        for (int sj : s) {
+            ans += cnt.getOrDefault(sj - k, 0);
+            cnt.merge(sj, 1, Integer::sum); // cnt[sj]++
+        }
+        return ans;
+    }
+}
+
+
+
+
+
+
 //官方题解：
 public class Solution {
     public int subarraySum(int[] nums, int k) {
