e5f8b500dc1813e2f11628f45cdc0a0a903bae91
FengYihan <1917460028@qq.com>
2026-03-07 14:33:33 +0800

    同余定理

--- Hot100plus/子串-和为K倍数子数组.java
+++ Hot100plus/子串-和为K倍数子数组.java
@@ -18,7 +18,48 @@ class Solution {
     }
 }
 
-作者：宫水三叶
-链接：https://leetcode.cn/problems/continuous-subarray-sum/solutions/808246/gong-shui-san-xie-tuo-zhan-wei-qiu-fang-1juse/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+//官方题解：
+
+/*
+--同余定理：如果两个整数m、n满足n-m能被k整除，那么n和m对k同余；
+
+即：若 ( pre(j) - pre (i) ) % k == 0 ，则 pre(j) % k == pre(i) % k
+
+又有：pre (i) % k = (a0 + a1 + ... + ai) % k = (a0 % k + a1 % k + ... ai % k ) % k
+
+--哈希表：Key ：pre(i) % k；Value： i
+
+--遍历过程：
+1、计算前缀和 pre( j ) % k
+2、当pre(j) % k 在哈希表中已存在，则说明此时存在 i 满足 pre(j) % k == pre(i) % k ( i < j )；
+在HashMap里，已知Key，可以取到Value 即i的值， 最后 判断 j - i >= 2 是否成立 即可。
+3、当 pre(j) % k 不存在于哈希表，则将 (pre(j) % k, j ) 存入哈希表。因在计算 pre(i) = (pre(i-1) + nums[i]) % k 时，pre(i) 只与上一个状态有关，
+故可以直接用变量pre 替代数组。 那么 (求前缀和 % k) 的公式就简化为 题解代码中的 remainder = (remainder + nums[i]) % k;
+
+*/
+
+class Solution {
+    public boolean checkSubarraySum(int[] nums, int k) {
+        int m = nums.length;
+        if (m < 2) {
+            return false;
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        map.put(0, -1);
+        int remainder = 0;
+        for (int i = 0; i < m; i++) {
+            remainder = (remainder + nums[i]) % k;
+            if (map.containsKey(remainder)) {
+                int prevIndex = map.get(remainder);
+                if (i - prevIndex >= 2) {
+                    return true;
+                }
+            } else {
+                map.put(remainder, i);
+            }
+        }
+        return false;
+    }
+}
