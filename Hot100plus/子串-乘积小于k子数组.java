2eb8edc910ad2ffea8a10102e1cb9c61477bf0d3
FengYihan <1917460028@qq.com>
2026-03-11 15:52:56 +0800

    官方题解

--- Hot100plus/子串-乘积小于k子数组.java
+++ Hot100plus/子串-乘积小于k子数组.java
@@ -1,4 +1,20 @@
+//官方题解（滑动窗口）
 
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int n = nums.length, ret = 0;
+        int prod = 1, i = 0;
+        for (int j = 0; j < n; j++) {
+            prod *= nums[j];
+            while (i <= j && prod >= k) {
+                prod /= nums[i];
+                i++;
+            }
+            ret += j - i + 1;
+        }
+        return ret;
+    }
+}
 
 
 
