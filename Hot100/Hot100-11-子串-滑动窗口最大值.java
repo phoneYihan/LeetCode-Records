a3ec7f25b33a1a4892d676c38b1b55af3cb3c57c
FengYihan <1917460028@qq.com>
2026-03-25 19:51:41 +0800

    大根堆-官方题解

--- Hot100/Hot100-11-子串-滑动窗口最大值.java
+++ Hot100/Hot100-11-子串-滑动窗口最大值.java
@@ -1,3 +1,38 @@
+//官方题解一，【优先队列】
+/*
+使用 大根堆 实时维护一系列元素中的最大值。
+
+为了方便判断堆顶元素与滑动窗口的位置关系，我们可以在优先队列中存储二元组 (num,index)，表示元素 num 在数组中的下标为 index。
+*/
+
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>(new Comparator<int[]>() {
+            public int compare(int[] pair1, int[] pair2) {
+                return pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1];
+            }
+        });
+        for (int i = 0; i < k; ++i) {
+            pq.offer(new int[]{nums[i], i});
+        }
+        int[] ans = new int[n - k + 1];
+        ans[0] = pq.peek()[0];
+        for (int i = k; i < n; ++i) {
+            pq.offer(new int[]{nums[i], i});
+            while (pq.peek()[1] <= i - k) {
+                pq.poll();
+            }
+            ans[i - k + 1] = pq.peek()[0];
+        }
+        return ans;
+    }
+}
+
+
+
+
+
 
 
 class Solution {
